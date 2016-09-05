eb2c2d9bbd0f74da00e8d0075a89e7da243dbec3
toddler95 <bishalpaul1905@gmail.com>
2016-09-06 02:55:58 +0530

    Minor Changes for Category Details

--- hozbee_beta/products/serializers.py
+++ hozbee_beta/products/serializers.py
@@ -15,6 +15,6 @@ class LaundryCatalogueItemSerializer(serializers.ModelSerializer):
 class FoodCategorySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = FoodCategory
-		fields = ( 'category','category_name','food' )
+		fields = ( 'category_id','category_name','food' )
 
 
